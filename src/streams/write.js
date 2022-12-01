78f6a0446adce17efe188e6bcfc06fb8ce972676
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 23:48:58 +0300

    feat: реализована функция, которая записывает потоковые данные в файл

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'node:process';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'fileToWrite.txt']);
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(sourcePath);
+    stdin.pipe(stream);
 };
 
 await write();
