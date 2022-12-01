d6834c062771329af5eb7cef10daa20c71a0272f
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 18:00:32 +0300

    feat: реализована функция создания файла

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { writeFile } from 'node:fs/promises';
+import { getPath } from "../helpers/helpers.js";
+
+const freshMsg = 'I am fresh and young';
+const freshFile = getPath(import.meta.url, ['files', 'fresh.txt']);
+
+
 const create = async () => {
-    // Write your code here 
+    try {
+      await writeFile(freshFile, freshMsg, {flag: 'wx'});
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 await create();
