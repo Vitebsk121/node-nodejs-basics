09e3d56e4dfd0b2ed7b20beaac6425d45448b60b
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 23:54:16 +0300

    feat: реализована функция, которая переворачивает текст с помощью потока Transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,31 @@
+import { Transform, pipeline } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+import { EOL } from 'os';
+
+class textTransform extends Transform {
+  _transform(chunk, encoding, callback) {
+    try {
+      const data = chunk.toString()
+      const reversedData = data.replace(EOL, '').split('').reverse().join('');
+
+      callback(null, reversedData + EOL);
+    } catch (e) {
+      callback(e);
+    }
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reversesText = new textTransform();
+
+    pipeline(
+      stdin,
+      reversesText,
+      stdout,
+      err => {
+        console.log(err);
+      },
+    );
 };
 
 await transform();
