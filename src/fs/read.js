5949146d558792cbf796cff8b4df9ce3edac008e
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 18:02:02 +0300

    feat: реализована функция чтения содержимого файла

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs/promises';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'fileToRead.txt']);
+
 const read = async () => {
-    // Write your code here 
+    try {
+      console.log(await readFile(sourcePath, "utf-8"));
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 await read();
