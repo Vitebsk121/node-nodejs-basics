45770ab42317f1ceea1634c518602c40b12c89bf
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 18:01:22 +0300

    feat: реализована функция переименования файла и смены расширения

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as renameNode } from 'node:fs/promises';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'wrongFilename.txt']);
+const targetPath = getPath(import.meta.url, ['files', 'properFilename.md']);
+
 const rename = async () => {
-    // Write your code here 
+    try {
+      await renameNode(sourcePath, targetPath);
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 await rename();
