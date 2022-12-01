f50fb3c0287516977f9dacd1d52c4cffd8391f2e
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 18:01:37 +0300

    feat: реализована функция удаления файла

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { rm } from 'node:fs/promises';
+import { getPath } from "../helpers/helpers.js";
+
+const target = getPath(import.meta.url, ['files', 'fileToRemove.txt']);
+
 const remove = async () => {
-    // Write your code here 
+    try {
+      await rm(target);
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 await remove();
