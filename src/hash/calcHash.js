f631f5c24299e405430781652a046d35f392ecf9
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 22:19:02 +0300

    feat: реализована функция, которая вычисляет хэш

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'fileToCalculateHashFor.txt']);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+      const fileData = await readFile(sourcePath);
+      const hash = createHash('sha256').update(fileData).digest('hex');
+      console.log(hash);
+    } catch (e) {
+      throw new Error(e);
+    }
 };
 
 await calculateHash();
