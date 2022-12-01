7e9eea4913e06fa7e5b19951eb631e459e67e82a
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 19:17:48 +0300

    feat: реализована функция, которая анализирует аргументы командной строки

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+import { argv } from 'node:process';
+
+
 const parseArgs = () => {
-    // Write your code here 
+    const resultArgs = [];
+
+    const cliArgs = argv.slice(2);
+
+    cliArgs.forEach((arg, index) => {
+      const valueIndex = index + 1;
+
+      if (arg.startsWith('--')) {
+        resultArgs.push(arg + ' is ' + cliArgs[valueIndex]);
+      }
+    });
+
+    console.log(resultArgs.join(', '));
 };
 
 parseArgs();
