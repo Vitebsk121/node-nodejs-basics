0c63ef7f0bf89e7a0bd6c24227fe0441cca53dda
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 19:17:10 +0300

    feat: реализована функция анализирования переменных окружения

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvVariables = [];
+
+    for(const [key, envVariables] of Object.entries(env)) {
+      if (key.startsWith('RSS_')) {
+        rssEnvVariables.push(key + '=' + envVariables);
+      }
+    }
+
+    console.log(rssEnvVariables.join('; '));
 };
 
 parseEnv();
