09de8d2a697a510e382e2eeaae2ed7703cb93393
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 18:00:53 +0300

    feat: реализована функция копирования папки

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files']);
+const targetPath = getPath(import.meta.url, ['files_copy']);
+
 const copy = async () => {
-    // Write your code here 
+    try {
+      const sourceFiles = await readdir(sourcePath);
+      await mkdir(targetPath);
+      sourceFiles.forEach((fileNm) => {
+        const source = sourcePath + '/' + fileNm;
+        const target = targetPath + '/' + fileNm
+        copyFile(source, target);
+      })
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 copy();
