390af8617813e964fc272ccecc45ca13bc5964cd
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-01 23:48:22 +0300

    feat: реализована функция, которая читает содержимое файла с помощью стрима.

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'node:process';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'fileToRead.txt']);
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(sourcePath);
+    stream.pipe(stdout);
 };
 
 await read();
