2b9560034474e9d2bf8ec54d43d36f02211801e5
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-02 17:31:46 +0300

    feat: реализована функция, которая разархивирует архив

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'node:stream';
+import { getPath } from "../helpers/helpers.js";
+
+const sourcePath = getPath(import.meta.url, ['files', 'archive.gz']);
+const targetPath = getPath(import.meta.url, ['files', 'fileToCompress.txt']);
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(targetPath);
+    const gzip = createUnzip();
+
+    pipeline(readStream, gzip, writeStream, err => {if (err) console.log(err)});
 };
 
 await decompress();
