a7b3bd8177022bd5bafa55f991890c61adf22b5e
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-03 01:25:57 +0300

    feat: реализована функция, которая создает несколько рабочих потоков

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { cpus } from 'os';
+import { Worker } from 'node:worker_threads';
+import { getPath } from "../helpers/helpers.js";
+
+const workerFuncPath = getPath(import.meta.url, ['worker.js']);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCoreCount = cpus();
+    const workersList = cpuCoreCount.map((_, i) => {
+      const workerData = 10 + i;
+      return  new Promise((res, rej) => {
+        const worker = new Worker(workerFuncPath, { workerData });
+
+        worker.on("message", (value) => res(value));
+        worker.on('error', (err) => rej(err));
+      })
+    });
+
+    const workersStateList = await Promise.allSettled(workersList);
+
+    const result = workersStateList.map(({ status, value }) => (
+      {
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null,
+      }
+    ))
+
+    console.log(result);
+
 };
 
 await performCalculations();
