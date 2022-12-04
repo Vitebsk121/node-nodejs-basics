e459e984572839478d49d4134d235fbee0c6137c
Демченко Александр <aleksandr.demchenko@glowbyteconsulting.com>
2022-12-04 21:12:35 +0300

    feat: реализована функция, которая создает IPC-канал между главным и дочерним процессами

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { spawn } from 'node:child_process'
+import { getPath } from "../helpers/helpers.js";
+
+const childProcessFuncPath = getPath(import.meta.url, ['files', 'script.js']);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [childProcessFuncPath, ...args]);
+
+    process.stdin.on('data', (chunk) => {
+        childProcess.stdin.write(chunk);
+    });
+
+    childProcess.stdout.on('data', (data) => console.log(data.toString()));
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
